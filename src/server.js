e255dcc4fcd7311ce097c66f1b9204ddfe8383a5
hangduchuy <duchuy246938@gmail.com>
2024-04-13 22:12:07 +0700

    dsa

--- src/server.js
+++ src/server.js
@@ -3,13 +3,13 @@ import bodyParser from "body-parser";
 import viewEngine from "./configs/viewEngine";
 import webRoutes from "./routes/web";
 
-const cors = require('cors')
+const cors = require("cors");
 let app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.set("views", "./views");
 //config view Engine
 viewEngine(app);
 
@@ -19,5 +19,5 @@ webRoutes(app);
 let port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log("App is running at the port: " + port);
-})
+  console.log("App is running at the port: " + port);
+});
